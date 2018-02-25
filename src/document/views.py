40dab45ca7b0352d72ae1a07d40eac359bc6c97e
kostet19940308 <kostet19940308@gmail.com>
2018-02-25 16:47:05 +0300

    RestApi:add simple api for documents

--- src/document/views.py
+++ src/document/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets, permissions
+from .serializers import DocumentSerializer
+from .models import Document
 # Create your views here.
+
+
+class DocumentViewSet(viewsets.ModelViewSet):
+    queryset = Document.objects.all()
+    serializer_class = DocumentSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(folder_id=self.kwargs["chat_id"], author=self.request.user)
+
+    def get_queryset(self):
+        q = self.queryset
+        if 'folder' in self.request.query_params:
+            q = q.filter(folder=self.request.query_params['folder'])
+        return q
