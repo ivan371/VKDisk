7a7e1e3d72453d1a616aaf4e684864963bd523e5
lesina <lesha_lesovoy@mail.ru>
2018-03-06 00:51:05 +0300

    docViewer: new field with vk url

--- src/document/serializers.py
+++ src/document/serializers.py
@@ -1,5 +1,5 @@
 from .models import Document
-from rest_framework import serializers, viewsets
+from rest_framework import serializers, viewsets, fields
 from core.serializers import UserSerializer
 from folder.serializers import FolderSerializer
 
@@ -8,7 +8,18 @@ class DocumentSerializer(serializers.HyperlinkedModelSerializer):
     folder = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
     id_owner = serializers.IntegerField(read_only=True)
     id_source = serializers.IntegerField(read_only=True)
+    vk_url = fields.SerializerMethodField('get_url')
 
     class Meta:
         model = Document
-        fields = ('id', 'title', 'author', 'folder', 'type', 'id_owner', 'id_source')
+        fields = ('id', 'title', 'author', 'folder', 'type', 'id_owner', 'id_source', 'vk_url')
+
+    def get_url(self, obj):
+        base, query = obj.vk_doc.url.split("?")
+        query = query.split("&")
+        for i in range(len(query)):
+            if "no_preview" in query[i]:
+                query.pop(i)
+                break
+        query = "&".join(query)
+        return base + "?" + query
