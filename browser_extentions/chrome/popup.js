c4b84da11c2878662b34639e3d393d20b44bd7c2
Aleksey <lesha_lesovoy@mail.ru>
2018-03-18 21:59:12 +0300

    Update popup.js
    Joder! В Пизду комментарии

--- browser_extentions/chrome/popup.js
+++ browser_extentions/chrome/popup.js
@@ -2,19 +2,6 @@ SITE_URL = "http://localhost:8000";
 AUTH_URLS = SITE_URL + "/accounts/oauth_urls/";
 
 function loadVKAuth(vk_url) {
-    // chrome.tabs.create({url: SITE_URL + vk_url, active: true}, (tab) => {
-    //     let authTabId = tab.id;
-    //     chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    //             if (tabId == authTabId && changeInfo.url != undefined) {
-    //                 if (changeInfo.url.indexOf('oauth.vk.com/blank.html') > -1) {
-    //                     let url = changeInfo.url.split('#')[1];
-    //                     let params = parseQuery(url);
-    //                     fetch_login_form(params.code, params.state);
-    //                 }
-    //             }
-    //         }
-    //     );
-    // });
     chrome.runtime.sendMessage({vk_url: vk_url, send_vk_request: true});
 }
 
