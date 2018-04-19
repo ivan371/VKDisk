4614f6f63d37e6f3fd04146005041f9e814f8f21
Vladimir Panov <vladimir.panov@phystech.edu>
2018-04-19 21:21:04 +0300

    LoadForwardedMessages: fix created time

--- src/document/models.py
+++ src/document/models.py
@@ -10,6 +10,7 @@ from vk_api_wrapper.models import DocAttachment
 
 
 class Document(Named, Authored, Dated):
+    created = models.DateTimeField(verbose_name=_(u'datetime'))
     id_elastic = models.CharField(max_length=1024, default="1")
     type = models.CharField(max_length=128, blank=False, default=_(u'file'), verbose_name=_(u'file'))
     folder = models.ForeignKey(Folder, on_delete=models.CASCADE, null=True, blank=True, verbose_name=_(u"folder"))
