d980ccc2ba40a1595301f89b1e0a279c361698f5
ALTAIRvovan <vladimir.panov@phystech.edu>
2018-04-22 23:58:47 +0300

    Load forwarded messages (#39)
    * Fullstack 10 (#35)
    
    * fullstack-10 add change sort
    
    * fullstack-10 loader and sorting
    
    * testAndFix: add etc configs. Add new concurrent lib
    
    * testAndFix: make load_text task for loading data from file.
    Move tasks to different queues
    
    * testAndFix: removed mysqlclient, bleat !!! add default ordering. remove parser docs!
    
    * LoadForwardedMessages: add is_owner field to Document model
    
    * LoadForwardedMessages: fix created time
    
    * add celery configs

--- src/celery_run.sh
+++ src/celery_run.sh
@@ -1,1 +1,22 @@
-celery -A application worker -l info -B
+#!/bin/bash
+
+declare -a cmd_list=(
+    "celery -A application worker -l info -P eventlet -c 4 -Q vk,default"
+    "celery -A application worker -l info -B -Q beat"
+    "celery -A application worker -l info -P eventlet -c 2 -Q text"
+)
+
+for cmd in "${cmd_list[@]}"; do {
+  echo "Process \"$cmd\" started";
+  $cmd & pid=$!
+  PID_LIST+=" $pid";
+} done
+
+trap "kill $PID_LIST" SIGINT
+
+echo "Parallel processes have started";
+
+wait $PID_LIST
+
+echo
+echo "All processes have completed";
--- src/vk_api_wrapper/pipline.py
+++ src/vk_api_wrapper/pipline.py
@@ -1,6 +1,12 @@
 from vk_api_wrapper.tasks import download_dialog_list, download_user_documents
 
 
+def add_vk_id_to_user(strategy, uid, user, is_new=False, *args, **kwargs):
+    if is_new or not user.vk_id:
+        user.vk_id = uid
+        strategy.storage.user.changed(user)
+
+
 def load_user_dialogs(backend, details, response, uid, user, *args, **kwargs):
     social = kwargs.get('social') or \
              backend.strategy.storage.user.get_social_auth(backend.name, uid)
